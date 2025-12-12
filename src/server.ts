308748facfc2a1a03093e747bc84d4f48218eb97
yaseenAkhtar <muhammadyaseenakhtar17@gmail.com>
2025-12-12 13:19:16 +0500

    add winston for logging; update server to use logger and include NODE_ENV in config

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import app from "./app.ts";
 import { Config } from "./config/index.ts";
+import logger from "./config/logger.ts";
 
 const startServer = () => {
     try {
         const PORT = Config.PORT;
 
-        app.listen(PORT, () =>
-            console.log(`app is listening at port: ${PORT}`),
-        );
+        app.listen(PORT, () => {
+            logger.warn("Warning testing");
+            logger.error("Error testing");
+            logger.info(`Server running on port ${PORT}`, {
+                port: PORT,
+            });
+        });
     } catch (error) {
         console.error(error);
         process.exit(1);
