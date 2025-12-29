9b444011bdd2776a37555ba5e5bd4259c2857a26
yaseenAkhtar <muhammadyaseenakhtar17@gmail.com>
2025-12-29 13:37:46 +0500

    fix: Enhance error handling in UserService create method to throw specific error on database failure

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,15 +1,24 @@
 import type { Repository } from "typeorm";
 import { User } from "../entity/User.ts";
 import type { UserData } from "../types/index.ts";
+import createHttpError from "http-errors";
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
     async create({ firstName, lastName, email, password }: UserData) {
-        return await this.userRepository.save({
-            firstName,
-            lastName,
-            email,
-            password,
-        });
+        try {
+            return await this.userRepository.save({
+                firstName,
+                lastName,
+                email,
+                password,
+            });
+        } catch {
+            const error = createHttpError(
+                500,
+                "Failed to store data in database!",
+            );
+            throw error;
+        }
     }
 }
