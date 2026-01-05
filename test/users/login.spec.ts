23f59baeacfb05dc9c9f71b4f602bc6798f0aca4
yaseenAkhtar <muhammadyaseenakhtar17@gmail.com>
2026-01-05 11:43:04 +0500

    add test for checking access & refresh token in cookies.

--- test/users/login.spec.ts
+++ test/users/login.spec.ts
@@ -5,6 +5,7 @@ import { AppDataSource } from "../../src/config/data-source.ts";
 import { User } from "../../src/entity/User.ts";
 import { Roles } from "../../src/constants/index.ts";
 import bcrypt from "bcrypt";
+import { isJwt } from "../utils/index.ts";
 
 describe("POST /auth/login", () => {
     let connection: DataSource;
@@ -51,6 +52,54 @@ describe("POST /auth/login", () => {
 
             expect(response.statusCode).toBe(200);
             expect(users).toHaveLength(1);
+        });
+
+        it("should return the access and refresh token inside a cookie", async () => {
+            //Arrange
+            const user = {
+                email: "yaseen@gmail.com",
+                password: "secretPassword",
+            };
+
+            const hashedPassword = await bcrypt.hash(user.password, 10);
+            const userRepository = connection.getRepository(User);
+            await userRepository.save({
+                ...user,
+                firstName: "Yaseen",
+                lastName: "Akhtar",
+                password: hashedPassword,
+                role: Roles.CUSTOMER,
+            });
+            await userRepository.find();
+
+            //Act
+            const response = await request(app).post("/auth/login").send(user);
+
+            //assert
+
+            const cookies: string[] | undefined = response.headers[
+                "set-cookie"
+            ] as unknown as string[] | undefined;
+            expect(cookies).toBeDefined();
+            expect(cookies!.length).toBeGreaterThanOrEqual(2);
+
+            // Check access token cookie
+            const accessTokenCookie = cookies!.find((cookie) =>
+                cookie.startsWith("accessToken="),
+            );
+            expect(accessTokenCookie).toBeDefined();
+            expect(accessTokenCookie).toMatch(/HttpOnly/);
+            // expect(accessTokenCookie).toMatch(/Secure/); // Ensure HTTPS in production
+            expect(isJwt(accessTokenCookie)).toBeTruthy(); // Check if it's a valid JWT)
+
+            // Check refresh token cookie
+            const refreshTokenCookie = cookies!.find((cookie) =>
+                cookie.startsWith("refreshToken="),
+            );
+            expect(refreshTokenCookie).toBeDefined();
+            expect(refreshTokenCookie).toMatch(/HttpOnly/);
+            // expect(refreshTokenCookie).toMatch(/Secure/); // Ensure HTTPS in production
+            expect(isJwt(refreshTokenCookie)).toBeTruthy(); // Check if it's a valid JWT)
         });
     });
 });
