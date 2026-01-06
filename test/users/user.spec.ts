bfb685eba69c59442f2b34e92a97d2400662cb9c
yaseenAkhtar <muhammadyaseenakhtar17@gmail.com>
2026-01-06 07:38:33 +0500

    add mock-jwks; error in tests due to configuration problem(mjs vs cjs); Be fixed.

--- test/users/user.spec.ts
+++ test/users/user.spec.ts
@@ -1,18 +1,31 @@
 import { DataSource } from "typeorm";
 import app from "../../src/app.ts";
 import request from "supertest";
+import { createJWKSMock } from "mock-jwks";
 import { AppDataSource } from "../../src/config/data-source.ts";
+import { User } from "../../src/entity/User.ts";
+import { Roles } from "../../src/constants/index.ts";
+
 describe("GET /auth/me", () => {
     let connection: DataSource;
+    let jwks: ReturnType<typeof createJWKSMock>;
 
     beforeAll(async () => {
+        jwks = createJWKSMock("http://localhost:1717");
         connection = await AppDataSource.initialize();
     });
 
     beforeEach(async () => {
+        //start jwks server
+        jwks.start();
         //Database drop
         await connection.dropDatabase();
         await connection.synchronize();
+    });
+
+    afterEach(() => {
+        //stop jwks server
+        jwks.stop();
     });
 
     afterAll(async () => {
@@ -21,16 +34,37 @@ describe("GET /auth/me", () => {
 
     describe("Valid Token provided", () => {
         it("should return 200 status", async () => {
-            //AAA rule => Arrange, Act, Assert
-
             //Arrange
+            // (1) --> Register User
+            const user = {
+                firstName: "Hasssan",
+                lastName: "akhtar",
+                email: "hassan@gmail.com",
+                password: "secretPass",
+            };
 
+            const userRepository = connection.getRepository(User);
+            const registeredUser = await userRepository.save({
+                ...user,
+                role: Roles.CUSTOMER,
+            });
+
+            // (2) --> Generate Token
+            const accessToken = jwks.token({
+                sub: String(registeredUser.id),
+                role: registeredUser.role,
+            });
             //Act
-            const response = await request(app).get("/auth/me").send();
+            const response = await request(app)
+                .get("/auth/me")
+                .set("Cookie", [`accessToken = ${accessToken}`])
+                .send();
 
             //Assert
-
-            expect(response.statusCode).toBe(200);
+            //check if user id matches with registered user
+            expect((response.body as Record<string, string>).id).toBe(
+                registeredUser.id,
+            );
         });
     });
 });
