5501039f80fb9178e56b23e36c5fb6d6337c8933
yaseenAkhtar <muhammadyaseenakhtar17@gmail.com>
2026-01-24 15:23:56 +0500

    test: enable Jest coverage reporting and add test:coverage script
    - Enable coverage collection in jest.config.mjs (v8 provider, TS sources)
    - Add test:coverage npm script to run Jest with coverage flags

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,7 +2,18 @@
 export default {
     preset: "ts-jest/presets/default-esm",
     testEnvironment: "node",
-
+    collectCoverage: true,
+    coverageProvider: "v8",
+    collectCoverageFrom: [
+        "src/**/*.ts",
+        "!src/**/index.ts",
+        "!src/config/**",
+        "!src/constants/**",
+        "!src/migration/**",
+        "!src/tests/**",
+        "!**/node_modules/**",
+        "!src/server.ts",
+    ],
     extensionsToTreatAsEsm: [".ts"],
 
     transform: {
